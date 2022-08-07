06611e0abb5b99815458620be7db1f3e2b7e1361
chxtio <chxtio@gmail.com>
2022-08-06 22:50:31 -0700

    Add SIK_Circuit_1B-Potentiometer

--- SIK_Circuit_1A-Blink/SIK_Circuit_1A-Blink.ino
+++ SIK_Circuit_1A-Blink/SIK_Circuit_1A-Blink.ino
@@ -29,7 +29,7 @@ void loop() {
 //  delay(2000);              // Wait for two seconds
 
   digitalWrite(13, HIGH);   // Turn on the LED
-  delay(500);              
+  delay(500);               
   digitalWrite(13, LOW);    // Turn off the LED
   delay(500); 
   digitalWrite(13, HIGH);   // Turn on the LED
